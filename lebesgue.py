aa70f1165455cf2b4f1995c095e7a199e20e1e9c
Pedro Sartori <pedrosartdr@gmail.com>
2025-12-13 00:31:28 -0300

    Implementa a definição de Lebesgue Integral

--- lebesgue.py
+++ lebesgue.py
@@ -15,35 +15,27 @@ ax.axvline(1, color='black', linestyle='dashed')
 ax.fill_between(x, y, color='none', edgecolor="#9f9f9f", hatch='....')
 
 #%%
-n = 40
-binwidth = np.random.uniform(0.1, 1, n)
-binwidth = binwidth/np.sum(binwidth)
-bins = np.empty(n+1)
-bins[0] = 0.0
-for i in range(len(binwidth)):
-    bins[i+1] = bins[i] + binwidth[i]
+bins = np.linspace(0, 3, 20)
 bins
 
 # %%
 ind = np.empty([len(x), len(bins)-1])
 c = np.empty(len(bins)-1)
 mu = np.empty(len(bins)-1)
+dx = x[1] - x[0]
 for i in range(len(bins)-1):
-    mask = (bins[i] <= x) & (x < bins[i+1])
-    c[i] = np.min(y[mask])
+    print(bins[i+1])
+    mask = (bins[i] < y) & (y < bins[i+1])
+    c[i] = bins[i]
     ind[:,i] = mask
-    mu[i] = bins[i+1]-bins[i]
-ind[-1,-1] = 1
+    mu[i] = mask.sum() * dx
 s = ind@c
-
 #%%
 print('Real AUC: ', np.trapz(y, x))
 print('Lebesgue: ', c@mu)
 
 # %%
 fig, ax = plt.subplots()
-for ci, bi in zip(c, bins[1:]):
-    ax.plot([bi, bi], [0, ci], color='#9f9f9f')
 ax.axhline(0, color='black', linestyle='dashed')
 ax.axvline(0, color='black', linestyle='dashed')
 ax.axvline(1, color='black', linestyle='dashed')
