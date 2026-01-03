7a0e78793874baac1b02689e0f3df0570181abb6
Pedro Sartori <pedrosartdr@gmail.com>
2026-01-02 23:30:45 -0300

    lebesgue2 faz o cálculo da integral

--- lebesgue2.py
+++ lebesgue2.py
@@ -16,10 +16,35 @@ plt.plot(x, y)
 x = np.linspace(0, 10, 1000)
 # y = 2.5-0.1*(x-5)**2
 y = 1+np.sin(x)
-plt.plot(x, y)
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 3), dpi=300)
+ax.plot(x, y, color='black')
+ax.set_ylabel(r'$f\,(\omega)$')
+ax.set_xlabel(r'$\omega$')
 
 # %%
-n = 3
+ns = [3, 5, 10, 100]
+
+fig, axs = plt.subplots(2, 2, figsize=(8, 6), constrained_layout=True, dpi=300)
+for i, (n, ax) in enumerate(zip(ns, axs.ravel())):
+    sets = pd.DataFrame({
+        'x': x,
+        'y': y,
+        'set': pd.cut(y, bins=n, right=False)
+    })
+    sets['s'] = sets.groupby('set')['y'].transform('max')
+
+    ax.set_title(f'$n={n}$', pad=12)
+    ax.fill_between(sets['x'], sets['s'], alpha=0.08, color='black', edgecolor='none')
+    ax.plot(sets['x'], sets['s'], alpha=0.4, color='black', label=r'$\varphi_{' + str(n) + r'}\,(\omega)$')
+    ax.plot(sets['x'], sets['y'], color='black', label=r'$f\,(\omega)$')
+    ax.set_xlabel('$\omega$')
+    ax.legend()
+    ax.set_ylabel('Valor da função')
+
+# %%
+n=100
+fig, ax = plt.subplots(1, 1, figsize=(7, 4), dpi=300)
 sets = pd.DataFrame({
     'x': x,
     'y': y,
@@ -27,12 +52,11 @@ sets = pd.DataFrame({
 })
 sets['s'] = sets.groupby('set')['y'].transform('max')
 
-fig, ax = plt.subplots(1, 1, figsize=(7, 4))
-ax.fill_between(sets['x'], sets['s'], alpha=0.1, color='black', edgecolor='none')
+dx = sets["x"].iloc[1] - sets["x"].iloc[0]
+L = float((sets["s"] * dx).sum())
+
 ax.plot(sets['x'], sets['s'], alpha=0.4, color='black', label=r'$\varphi_{' + str(n) + r'}\,(\omega)$')
 ax.plot(sets['x'], sets['y'], color='black', label=r'$f\,(\omega)$')
-ax.set_ylabel('Valor da função')
 ax.set_xlabel('$\omega$')
-ax.legend()
-
-# %%
+ax.set_ylabel('Valor da função')
+print(f"AUC={L}")
