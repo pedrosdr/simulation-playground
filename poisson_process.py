a76f470f92d02514b51ae9b452804a4ac1784880
Pedro Sartori <pedrosartdr@gmail.com>
2026-02-22 16:15:43 -0300

    poisson_process: Simula um processo de poisson aproximando uma soma de bernoullis com p=\mu/n

--- poisson_process.py
+++ poisson_process.py
@@ -1,7 +1,7 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import poisson
+from scipy.stats import poisson, bernoulli
 
 # %%
 T = 1000
@@ -12,7 +12,18 @@ incr = poisson.ppf(
     np.random.rand(T),
     mu = lbd*dt 
 )
-n = incr.cumsum()
-plt.plot(t, n)
+N = incr.cumsum()
+plt.plot(t, N)
+
+# %%
+mu = 10
+n = 1_000_000
+p = mu/n
+x = np.arange(n)
+incr = np.random.choice(
+    [1, 0], size=n, p=[p, 1-p]
+)
+N = np.cumsum(incr)
+plt.plot(x,N)
 
 # %%
