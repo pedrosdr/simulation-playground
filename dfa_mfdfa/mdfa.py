7be7a583fdac704a679fe69414cf9f525ddc126e
Pedro Sartori <pedrosartdr@gmail.com>
2026-03-02 00:34:22 -0300

    dfa: Implementa o RMSq

--- dfa_mfdfa/mdfa.py
+++ dfa_mfdfa/mdfa.py
@@ -6,7 +6,7 @@ import math
 # %%
 # Definindo os incrementos White Noise
 N=10000
-t = th.arange(1, N+1).to(th.float64)
+t = th.arange(N, dtype=th.float64)
 incr = th.randn(t.shape[0], dtype=t.dtype)
 plt.plot(t, incr)
 
@@ -24,26 +24,19 @@ def polyfit(x, y, deg=2):
     X = x.unsqueeze(-1)**degs
     betas = th.linalg.lstsq(X, y).solution.unsqueeze(-1)
     res = X@betas
-    return res.squeeze(-1)
+    return res.squeeze(-1), betas
 
 #%%
-plt.plot(t, polyfit(t, x, deg=2))
+plt.plot(t, polyfit(t, x, deg=2)[0])
 plt.plot(t, x)
 
 #%%
 # Calculando as flutuações para apenas uma escala
 scale = 10
 n_segments = math.floor(x.shape[0]/scale)
-idx_start = th.arange(n_segments)*scale
-idx_end = idx_start+scale
-segments = th.stack(
-    [x[s.item():e.item()] for s,e in zip(idx_start, idx_end)],
-    dim=0
-)
-intervals = th.stack(
-    [t[s.item():e.item()] for s, e in zip(idx_start, idx_end)]
-)
-trends = polyfit(intervals, segments, deg=2)
+segments = x.unfold(0, scale, scale)
+intervals = t.unfold(0, scale, scale)
+trends, _ = polyfit(intervals, segments, deg=2)
 dtr_segments = segments-trends
 F = th.sqrt(th.mean(dtr_segments**2))
 
@@ -57,22 +50,17 @@ for itv, seg in zip(intervals, dtr_segments):
 
 # %%
 # Definindo uma função para o cálculo das flutuações
-def fluctuation(scale):
-    scale = int(scale.item())
-    n_segments = math.floor(x.shape[0]/scale)
-    idx_start = th.arange(n_segments)*scale
-    idx_end = idx_start+scale
-    segments = th.stack(
-        [x[s.item():e.item()] for s,e in zip(idx_start, idx_end)],
-        dim=0
-    )
-    intervals = th.stack(
-        [t[s.item():e.item()] for s, e in zip(idx_start, idx_end)]
-    )
-    trends = polyfit(intervals, segments, deg=2)
+def fluctuation(scale, x, q=2):
+    scale = int(scale)
+    t = th.arange(x.shape[0], dtype=x.dtype)
+    segments = x.unfold(0, scale, scale)
+    intervals = t.unfold(0, scale, scale)
+    trends, _ = polyfit(intervals, segments, deg=2)
     dtr_segments = segments-trends
-    return th.sqrt(th.mean(dtr_segments**2))
+    var_dtr_segs = th.mean(dtr_segments**2, axis=1)
+    return th.mean(var_dtr_segs**(q/2))**(1/q)
 
+fluctuation(1000, x, 2)
 
 # %%
 scales=th.unique(
@@ -83,7 +71,7 @@ scales=th.unique(
     ).round()
 ).to(int)
 
-F = th.stack([fluctuation(scale) for scale in scales])
+F = th.stack([fluctuation(scale, x) for scale in scales])
 plt.scatter(scales, F)
 plt.loglog()
 
@@ -98,3 +86,31 @@ alpha = betas[1]
 print(fr"alpha estimado = {alpha:.2f}")
 
 #%%
+# Definindo uma função para DFA
+def dfa(signal, q=2):
+    x = th.cumsum(signal - signal.mean(), dim=0)
+    scales=th.unique(
+        th.logspace(
+            th.log10(th.tensor(10.0)), 
+            th.log10(th.tensor(x.shape[0]//4).to(x.dtype)),
+            200
+        ).round()
+    ).to(int)
+    F = th.stack([fluctuation(scale, x, q=q) for scale in scales])
+    log_scales = th.log10(scales.to(x.dtype))
+    log_F = th.log10(F.to(x.dtype))
+    _, betas = polyfit(log_scales, log_F, deg=1)
+    return betas[1].item()
+
+# %%
+# testando com AR(1,1)
+x = th.randn(N+20)
+for i in range(1, x.shape[0]):
+    x[i] = x[i-1]*(-0.9)+x[i]*0.3
+x = x[20:]
+plt.plot(x)
+
+# %%
+qs = [-3, -2, -1, 1, 2, 3]
+plt.plot(qs, [dfa(x, q=q) for q in qs])
+# %%
