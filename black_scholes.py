4e34ca576fcf9f6e3b50633b4bfdb28b0ff48e9b
Pedro Sartori <pedrosartdr@gmail.com>
2026-04-16 18:57:21 -0300

    Faz uma simulação do modelo de B-S em português

--- black_scholes.py
+++ black_scholes.py
@@ -23,3 +23,22 @@ for i, E in enumerate(Es):
     plt.legend()
 
 # %%
+# Legenda em português
+r = 0.05
+sigma = 0.2
+Es = [5, 10, 20]
+linestyles = ['dotted', '-.', 'solid']
+fig, ax = plt.subplots(dpi=500, figsize=(6,4))
+for i, E in enumerate(Es):
+    u = (2*r)/(sigma**2)
+    C = E*u/(u+1)
+    S = np.linspace(C, 60, 1000)
+    G = (E/(u+1))*(((u+1)*S)/(E*u))**(-u)
+    ax.plot(
+        S, G, label=f'$K={E}$',
+        linestyle=linestyles[i],
+        color='black'
+    )
+    ax.set_ylabel('Preço da opção $put$ $(V)$')
+    ax.set_xlabel('Preço da ação $(S)$')
+    plt.legend()
